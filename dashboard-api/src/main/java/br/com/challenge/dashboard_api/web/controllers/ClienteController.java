b76c38d1516feb64328da9901696f0e70f049c3c
uLemos <fsnvag12@gmail.com>
2025-08-20 15:58:26 -0300

    :sparkles: :memo: :white_check_mark: Documentando ClienteController

--- dashboard-api/src/main/java/br/com/challenge/dashboard_api/web/controllers/ClienteController.java
+++ dashboard-api/src/main/java/br/com/challenge/dashboard_api/web/controllers/ClienteController.java
@@ -4,9 +4,13 @@ import java.util.List;
 import org.springframework.web.bind.annotation.*;
 import br.com.challenge.dashboard_api.application.services.DashboardService;
 import br.com.challenge.dashboard_api.web.dtos.ClienteDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/clientes")
+@Tag(name = "Clientes", description = "Endpoint para consulta de dados mestre de clientes")
 public class ClienteController {
 
   private final DashboardService dashboardService;
@@ -15,6 +19,8 @@ public class ClienteController {
     this.dashboardService = dashboardService;
   }
 
+  @Operation(summary = "Listar todos os Clientes", description = "Retorna uma lista com ID e nome de todos os clientes cadastrados.")
+  @ApiResponse(responseCode = "200", description = "Lista de clientes retornada com sucesso.")
   @GetMapping
   public List<ClienteDTO> findAllClientes() {
     return dashboardService.findAllClientes();
