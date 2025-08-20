8c60f670044e0e6075b354ce57a9c62368a63396
uLemos <fsnvag12@gmail.com>
2025-08-19 23:04:29 -0300

    :white_check_mark: Criando service para o controller

---
+++ dashboard-api/src/main/java/br/com/challenge/dashboard_api/application/services/DashboardService.java
@@ -0,0 +1,75 @@
+package br.com.challenge.dashboard_api.application.services;
+
+import br.com.challenge.dashboard_api.domain.entitys.Cliente;
+import br.com.challenge.dashboard_api.domain.entitys.Modulo;
+import br.com.challenge.dashboard_api.domain.entitys.Ticket;
+import br.com.challenge.dashboard_api.domain.repositories.ClienteRepository;
+import br.com.challenge.dashboard_api.domain.repositories.ModuloRepository;
+import br.com.challenge.dashboard_api.domain.repositories.TicketRepository;
+import br.com.challenge.dashboard_api.mappers.TicketMapper;
+import br.com.challenge.dashboard_api.web.dtos.CreateTicketDTO;
+import br.com.challenge.dashboard_api.web.dtos.DashboardDataDTO;
+import br.com.challenge.dashboard_api.web.dtos.TicketDTO;
+import jakarta.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DashboardService {
+  private final TicketRepository ticketRepository;
+  private final ClienteRepository clienteRepository;
+  private final ModuloRepository moduloRepository;
+  private final TicketMapper ticketMapper;
+
+  public DashboardService(TicketRepository ticketRepository, ClienteRepository clienteRepository,
+      ModuloRepository moduloRepository, TicketMapper ticketMapper) {
+    this.ticketRepository = ticketRepository;
+    this.clienteRepository = clienteRepository;
+    this.moduloRepository = moduloRepository;
+    this.ticketMapper = ticketMapper;
+  }
+
+  public DashboardDataDTO getDashboardData(int ano, int mes) {
+    List<Ticket> ticketsDb = ticketRepository.findByYearAndMonth(ano, mes);
+
+    List<TicketDTO> listaTicketsDTO = ticketsDb.stream()
+        .map(ticketMapper::toDTO)
+        .toList();
+
+    Map<String, Long> agrupadoPorCliente = ticketsDb.stream()
+        .collect(Collectors.groupingBy(ticket -> ticket.getCliente().getNome(), Collectors.counting()));
+
+    Map<String, Long> agrupadoPorModulo = ticketsDb.stream()
+        .collect(Collectors.groupingBy(ticket -> ticket.getModulo().getNome(), Collectors.counting()));
+
+    return DashboardDataDTO.builder()
+        .listaTickets(listaTicketsDTO)
+        .agrupadoPorCliente(agrupadoPorCliente)
+        .agrupadoPorModulo(agrupadoPorModulo)
+        .build();
+  }
+
+  public TicketDTO createTicket(CreateTicketDTO createTicketDTO) {
+    Cliente cliente = clienteRepository.findById(createTicketDTO.getClienteId())
+        .orElseThrow(
+            () -> new EntityNotFoundException("Cliente não encontrado com o ID: " + createTicketDTO.getClienteId()));
+
+    Modulo modulo = moduloRepository.findById(createTicketDTO.getModuloId())
+        .orElseThrow(
+            () -> new EntityNotFoundException("Módulo não encontrado com o ID: " + createTicketDTO.getModuloId()));
+
+    Ticket newTicket = new Ticket();
+    newTicket.setTitulo(createTicketDTO.getTitulo());
+    newTicket.setDataAbertura(LocalDateTime.now());
+    newTicket.setCliente(cliente);
+    newTicket.setModulo(modulo);
+
+    Ticket saveTicket = ticketRepository.save(newTicket);
+
+    return ticketMapper.toDTO(saveTicket);
+  }
+}
