67b9e74596d57cc1d1542aff8c30d5e9f8e68a62
uLemos <fsnvag12@gmail.com>
2025-08-19 22:56:57 -0300

    :construction: Commit inicial com a estrutura inicial

---
+++ dashboard-api/src/main/java/br/com/challenge/dashboard_api/DashboardApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge.dashboard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardApiApplication.class, args);
+	}
+
+}
---
+++ dashboard-api/src/test/java/br/com/challenge/dashboard_api/DashboardApiApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.challenge.dashboard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
