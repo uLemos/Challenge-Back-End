a9b34d6254f72ee3afc65235ff14cf52eb7c8eb0
uLemos <fsnvag12@gmail.com>
2025-08-21 18:29:40 -0300

    :white_check_mark: :recycle: Ajustando os DTOs existentes.

--- dashboard-api/src/main/java/br/com/challenge/dashboard_api/web/dtos/DashboardDataDTO.java
+++ dashboard-api/src/main/java/br/com/challenge/dashboard_api/web/dtos/DashboardDataDTO.java
@@ -2,13 +2,12 @@ package br.com.challenge.dashboard_api.web.dtos;
 
 import lombok.Builder;
 import lombok.Data;
-import java.util.List;
 import java.util.Map;
 
 @Data
 @Builder
 public class DashboardDataDTO {
-  private List<TicketDTO> listaTickets;
+  private PagedResultDTO<TicketDTO> listaTickets;
   private Map<String, Long> agrupadoPorCliente;
   private Map<String, Long> agrupadoPorModulo;
 }
