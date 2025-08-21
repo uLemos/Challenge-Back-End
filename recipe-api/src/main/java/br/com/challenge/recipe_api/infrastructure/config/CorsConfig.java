1c3de7de82645ed694cc93dc88ba2aff52b21303
uLemos <fsnvag12@gmail.com>
2025-08-21 12:36:02 -0300

    :wrench: :white_check_mark: Configurando CORS

---
+++ recipe-api/src/main/java/br/com/challenge/recipe_api/infrastructure/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package br.com.challenge.recipe_api.infrastructure.config;
+
+import org.springframework.lang.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(@NonNull CorsRegistry registry) {
+    registry.addMapping("/api/**")
+        .allowedOrigins("http://localhost:5173")
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+  }
+}
