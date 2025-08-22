734bd5f2a11079148e1e421fab740802ecde3134
uLemos <fsnvag12@gmail.com>
2025-08-21 22:46:35 -0300

    :recycle: :white_check_mark: Ajustando nossas chamadas a API e criando o método para buscar por mês

--- front-end/src/services/api.ts
+++ front-end/src/services/api.ts
@@ -2,10 +2,18 @@ import type { DashboardData, Cliente, Modulo } from "../types";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export async function getDashboardData(ano: number, mes: number, page: number = 0, size: number = 10): Promise<DashboardData> {
-  const response = await fetch(`${BASE_URL}/dashboard?ano=${ano}&mes=${mes}&page=${page}&size=${size}&sort=id,desc`);
+export async function getDashboardDataByMonthAndYear(ano: number, mes: number, page: number = 0, size: number = 10): Promise<DashboardData> {
+  const response = await fetch(`${BASE_URL}/dashboard/by-month-and-year?ano=${ano}&mes=${mes}&page=${page}&size=${size}&sort=id,desc`);
   if (!response.ok) 
-    throw new Error("Falha ao buscar dados do dashboard");
+    throw new Error("Falha ao buscar dados do dashboard por ano e mês");
+  return response.json();
+}
+
+export async function getDashboardDataByMonth(mes: number, page: number = 0, size: number = 10): Promise<DashboardData> {
+  const response = await fetch(`${BASE_URL}/dashboard/by-month?mes=${mes}&page=${page}&size=${size}&sort=id,desc`);
+  if (!response.ok) {
+    throw new Error('Falha ao buscar dados do dashboard por mês');
+  }
   return response.json();
 }
 
